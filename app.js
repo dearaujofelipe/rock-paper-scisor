8d5a9d0165097c9316d815f42471f81489bebad3
dearaujofelipe <mgdefelipe@outlook.com>
2023-09-12 14:19:58 -0300

    core logic

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSOR = 'SCISSOR';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -19,6 +22,31 @@ const getPlayerChoice = function () {
   return selection;
 };
 
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+};
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (cChoice === ROCK && pChoice === PAPER) ||
+    (cChoice === PAPER && pChoice === SCISSOR) ||
+    (cChoice === SCISSOR && pChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener('click', function () {
   if (gameIsRunning) {
     return;
@@ -26,5 +54,7 @@ startGameBtn.addEventListener('click', function () {
   gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+  console.log(winner);
 });
